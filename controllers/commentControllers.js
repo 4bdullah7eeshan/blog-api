77c3a9b8a24ec17dbcc12440aa0721f24000e1c0
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-11 13:49:50 +0530

    wip(r, c): declare controllers for comment
    Add logic later.

---
+++ controllers/commentControllers.js
@@ -0,0 +1,22 @@
+const asyncHandler = require("express-async-handler");
+
+const getCommentsByPostId = asyncHandler(async (req, res) => {
+    const { postId } = req.params; // post id will be needed
+
+});
+
+const createComment = asyncHandler(async (req, res) => {
+    const { postId } = req.params;
+
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+    const { id } = req.params; // id of the post
+
+});
+
+module.exports = {
+    getCommentsByPostId,
+    createComment,
+    deleteComment,
+};
