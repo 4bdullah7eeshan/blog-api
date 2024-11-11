cf6d3afb7406df3711fe770f273b218812f00954
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-11 15:29:06 +0530

    feat(jwt): secure comments routes with jwt

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const commentController = require("../controllers/commentControllers");
+const authenticateJwt = require("../middlewares/authenticateJwt");
 
 const commentRouter = Router({ mergeParams: true });
 
 commentRouter.get("/", commentController.getCommentsByPostId);
-commentRouter.post("/", commentController.createComment);
+commentRouter.post("/", authenticateJwt, commentController.createComment);
 commentRouter.get("/:commentId", commentController.getCommentById);
-commentRouter.delete("/:commentId", commentController.deleteComment);
+commentRouter.delete("/:commentId", authenticateJwt, commentController.deleteComment);
 
 module.exports = commentRouter;
