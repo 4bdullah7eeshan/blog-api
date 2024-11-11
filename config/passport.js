08a62ef0d987b54f382ba8a5321036e1fc666f9c
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-11 14:45:57 +0530

    feat: configure passport with prisma

---
+++ config/passport.js
@@ -0,0 +1,35 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+module.exports = (passport) => {
+  passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const user = await prisma.user.findUnique({ where: { username } });
+        if (!user) return done(null, false, { message: "User not found" });
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) return done(null, false, { message: "Incorrect password" });
+
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await prisma.user.findUnique({ where: { id } });
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  });
+};
