bc89a09937dedafdcb392229da0ec95ff04e1a18
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-11 13:21:23 +0530

    wip(r, c): use the routes in the app
    Routes used: user, posts, comments.

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 
+const userRouter = require("./routes/userRoutes");
+const postRouter = require("./routes/postRoutes");
+const commentRouter = require("./routes/commentRoutes");
+
 
 const app = express();
+
+app.use("/auth", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
