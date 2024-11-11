ea54d517b9fb6b47dfe82efd3bc90c7e251835ac
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-11 12:57:29 +0530

    refactor: update app name

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
-  console.log(`File Uploader running on port ${PORT}!`)
+  console.log(`Blog API running on port ${PORT}!`)
 );
